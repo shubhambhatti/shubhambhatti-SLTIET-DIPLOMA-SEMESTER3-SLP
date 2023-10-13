db088337896f0df3a730d005cfade6d9247c04bf
shubhambhatti <145088229+shubhambhatti@users.noreply.github.com>
2023-10-13 11:29:47 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+c=add(3,5)
+print(c)
+def add(a,b):
+    return a/b
+c=add(3,5)
+print(c)
+def add(a,b):
+    return a*b
+c=add(3,5)
+print(c)
+def add(a,b):
+    return a-b
+c=add(3,5)
+print(c)
